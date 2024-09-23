6fc4f0f7578f957f169827f5bcfae8fef96d3a7f
Dominik <mrdominino@gmail.com>
2024-09-23 20:35:08 +0200

    refactor: better import from customer management entities

---
+++ src/customer_management/domain/entities/contact_person/__init__.py
@@ -0,0 +1,4 @@
+from .contact_person import ContactPerson, ContactPersonReadOnly
+from .validators import at_least_one_preferred_contact_method
+
+__all__ = ["ContactPerson", "ContactPersonReadOnly", "at_least_one_preferred_contact_method"]
---
+++ src/customer_management/domain/entities/customer/__init__.py
@@ -0,0 +1,7 @@
+from .customer import Customer
+from .validators import at_least_one_contact_person
+
+__all__ = [
+    "Customer",
+    "at_least_one_contact_person",
+]
