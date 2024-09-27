acf86fd1296ea565b4b9e4d082a1312528b38073
Dominik <mrdominino@gmail.com>
2024-09-27 21:59:05 +0200

    refactor: change type hints on IO functions

--- src/building_blocks/infrastructure/file/io.py
+++ src/building_blocks/infrastructure/file/io.py
@@ -1,13 +1,14 @@
 import shelve
 from collections.abc import Iterator
 from contextlib import contextmanager
+from pathlib import Path
 
 
 @contextmanager
-def get_read_db(file_path: str) -> Iterator[shelve.Shelf]:
+def get_read_db(file_path: Path) -> Iterator[shelve.Shelf]:
     with shelve.open(file_path, "r") as f:
         yield f
 
 
-def get_write_db(file_path: str) -> shelve.Shelf:
+def get_write_db(file_path: Path) -> shelve.Shelf:
     return shelve.open(file_path, "c", writeback=True)
