439a0288b7217806643eeb31acb8c903e28932b1
Dominik <mrdominino@gmail.com>
2024-10-26 20:16:40 +0200

    feat: throw invalid filter field error in file filter service

--- src/building_blocks/infrastructure/file/filters.py
+++ src/building_blocks/infrastructure/file/filters.py
@@ -3,21 +3,29 @@ from operator import attrgetter
 from typing import Any, Callable, TypeVar
 
 from building_blocks.application.filters import BaseFilterResolver, FilterCondition, FilterConditionType
+from building_blocks.infrastructure.exceptions import InvalidFilterField
 
 EntityT = TypeVar("EntityT")
 FilterFunc = Callable[[type[EntityT], str, Any], bool]
 
 
+def _get_field_value(obj: EntityT, field_name: str) -> Any:
+    try:
+        return attrgetter(field_name)(obj)
+    except AttributeError as e:
+        raise InvalidFilterField(field_name) from e
+
+
 def equals(entity: EntityT, field: str, value: Any) -> bool:
-    return value == attrgetter(field)(entity)
+    return value == _get_field_value(entity, field)
 
 
 def iequals(entity: EntityT, field: str, value: Any) -> bool:
-    return value.lower() == attrgetter(field)(entity).lower()
+    return value.lower() == _get_field_value(entity, field).lower()
 
 
 def search(entity: EntityT, field: str, value: Any) -> bool:
-    return value.lower().replace(" ", "") in attrgetter(field)(entity).lower().replace(" ", "")
+    return value.lower().replace(" ", "") in _get_field_value(entity, field).lower().replace(" ", "")
 
 
 class FileFilterResolver(BaseFilterResolver[FilterFunc]):
--- tests/infrastructure/file/building_blocks/test_filters.py
+++ tests/infrastructure/file/building_blocks/test_filters.py
@@ -1,7 +1,9 @@
 import pytest
 from attrs import define
 
+from building_blocks.application.exceptions import InvalidFilterType
 from building_blocks.application.filters import FilterCondition, FilterConditionType
+from building_blocks.infrastructure.exceptions import InvalidFilterField
 from building_blocks.infrastructure.file.filters import FileFilterService
 
 
@@ -23,6 +25,32 @@ def filter_service() -> FilterService:
 @pytest.fixture()
 def model() -> Model:
     return Model(field_1="some string", field_2="SoMe OtHeR StRinG")
+
+
+def test_invalid_field_raises_exception(filter_service: FilterService, model: Model) -> None:
+    filters = [
+        FilterCondition(
+            field="invalid field",
+            value="some string",
+            condition_type=FilterConditionType.EQUALS,
+        )
+    ]
+
+    with pytest.raises(InvalidFilterField):
+        filter_service.apply_filters(entity=model, filters=filters)
+
+
+def test_invalid_condition_type_raises_exception(filter_service: FilterService, model: Model) -> None:
+    filters = [
+        FilterCondition(
+            field="field_1",
+            value="some string",
+            condition_type="invalid type",
+        )
+    ]
+
+    with pytest.raises(InvalidFilterType):
+        filter_service.apply_filters(entity=model, filters=filters)
 
 
 def test_filter_equals_with_match_should_return_true(filter_service: FilterService, model: Model) -> None:
