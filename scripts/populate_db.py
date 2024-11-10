7b63df3e4b95ef008b7263c4bbc8a92e79c29420
Dominik <mrdominino@gmail.com>
2024-11-10 12:43:14 +0100

    fix: fix module base name in populate-db script

--- scripts/populate_db.py
+++ scripts/populate_db.py
@@ -14,7 +14,7 @@ def cli() -> None:
 @click.argument("persistence_engine", type=click.Choice(ALLOWED_PERSISTENCE_ENGINES, case_sensitive=False))
 def populate_db(persistence_engine: str) -> None:
     """Populate database with example data."""
-    module_name = f"fixtures.{persistence_engine}"
+    module_name = f"scripts.fixtures.{persistence_engine}"
     import_module(module_name)
 
 
